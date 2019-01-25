09423b11375ed958d1f174b50b2aa396d9de5303
Tommy Stigen Olsen <tommysolsen@gmail.com>
2019-01-26 00:13:29 +0100

    Implement Overheating of Boosters

--- Assets/Scripts/Thrusters/BoosterThrust.cs
+++ Assets/Scripts/Thrusters/BoosterThrust.cs
@@ -8,19 +8,41 @@ public class BoosterThrust : ThrustGenerator
     public float normalThrust = 1f;
     public float boostedThrust = 1f;
 
+    private bool depleted = false;
+    private bool firingBooster = false;
+    public float boostTime = 5f;
+    private float maxBoostTime;
+
+    public float boostRechargeRate = 0.5f;
+    public float boostReactivationDelay = 0.5f;
     public void Start() {
         this.thrust = this.normalThrust;
+        this.maxBoostTime = this.boostTime;
     }
 
     public void Update() {
+        if(firingBooster && !depleted) {
+            boostTime -= Time.deltaTime;
+            if(boostTime < 0) {
+                depleted = true;
+                firingBooster = false;
+                this.FireThrusters(false);
+            } 
+        } else if (boostTime < maxBoostTime) {
+            boostTime += Time.deltaTime * boostRechargeRate;
+            if(depleted && boostTime > maxBoostTime * boostReactivationDelay) {
+                depleted = false;
+            }
+        }
         if(Input.GetKeyUp(BoostButton) || Input.GetKeyDown(BoostButton)) {
+            this.firingBooster = Input.GetKey(BoostButton);
             this.FireThrusters(Input.GetKey(BoostButton));
             this.SendMessageUpwards("updateThrust", null, SendMessageOptions.DontRequireReceiver);
         }
     }
     public void FireThrusters(bool fireing) {
         bool isFiring = (bool)fireing;
-        switch(isFiring) {
+        switch(isFiring && !depleted) {
             case true:
                 this.thrust = boostedThrust;
                 break;
