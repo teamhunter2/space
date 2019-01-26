c8cd08088e4006a400ae2591c7fecac153e0c8b9
Thomas Alrek <thomas@alrek.no>
2019-01-26 14:17:20 +0100

    Update stuff

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+  public float thrust = 1f;
+  public float rotationSpeed = 100f;
+
+  void Update()
+  {
+    Transform player = GameManager.instance.player;
+    Vector3 target = player.position - transform.position;
+    float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+    Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+
+    transform.position += (player.position - transform.position).normalized * (Time.deltaTime * thrust);
+  }
+}
---
+++ Assets/MissileLauncher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileLauncher : MonoBehaviour
+{
+  public GameObject missile;
+  public int ammo = 10;
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      if (ammo > 0)
+      {
+        GameObject current = GameObject.Instantiate(missile);
+      }
+    }
+  }
+}
